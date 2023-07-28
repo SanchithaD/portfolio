5dbe43c7e38c030f1585f5bdfab7b4734b466022
Sanchitha Dinesh <sanchunila@gmail.com>
2023-07-28 11:30:39 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -61,7 +61,7 @@ export default function About() {
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="./Sanchu.jpg"
+              src="./sanchu.jpg"
             />
           </div>
         </div>
